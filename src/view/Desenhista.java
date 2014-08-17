9ae02e6799f8905c9d71ed218e2238d47acf1ae1
Thiago Rose <thiago.senhorinha@gmail.com>
2014-08-16 23:29:06 -0300

    Adicionando classe para desenhar formas

---
+++ src/view/Desenhista.java
@@ -0,0 +1,40 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import modelo.Poligono;
+import modelo.Ponto;
+import modelo.Reta;
+
+public class Desenhista {
+
+	private final Graphics2D grafico;
+
+	public Desenhista(Graphics2D grafico) {
+		this.grafico = grafico;
+	}
+
+	public void desenhar(Reta reta, Color cor) {
+		this.mudarCor(cor);
+		int[] coordenadas = reta.getCoordenadasDoPontos();
+		grafico.drawLine(coordenadas[0], coordenadas[1], coordenadas[2],
+				coordenadas[3]);
+	}
+
+	public void desenhar(Ponto ponto, Color cor) {
+		this.mudarCor(cor);
+		grafico.drawLine(ponto.getX(), ponto.getY(), ponto.getX(), ponto.getY());
+	}
+
+	public void desenhar(Poligono poligono, Color cor) {
+		for (Reta reta : poligono.getRetas()) {
+			desenhar(reta, cor);
+		}
+	}
+
+	private void mudarCor(Color cor) {
+		grafico.setColor(cor);
+	}
+
+}
