9d1892e1b8960d241c9f2b5ed4dadaba57fb5db0
Thiago Rose <thiago.senhorinha@gmail.com>
2014-08-16 23:26:58 -0300

    Adicionando método para fabricar poligonos

--- src/modelo/FabricaObjetos.java
+++ src/modelo/FabricaObjetos.java
@@ -2,24 +2,29 @@ package modelo;
 
 public class FabricaObjetos {
 
-	static FabricaObjetos fabrica = null;
+	private static FabricaObjetos fabrica;
 
 	private FabricaObjetos() {
 
 	}
 
-	static FabricaObjetos obterInstancia() {
-		if (fabrica == null)
+	public static FabricaObjetos obterInstancia() {
+		if (fabrica == null) {
 			return new FabricaObjetos();
+		}
 		return fabrica;
 	}
 
-	Ponto criarPonto(int x, int y, String nome) {
+	public Ponto criarPonto(int x, int y, String nome) {
 		return new Ponto(x, y, nome);
 	}
 
-	Reta criarReta(Ponto a, Ponto b, String nome) {
+	public Reta criarReta(Ponto a, Ponto b, String nome) {
 		return new Reta(a, b, nome);
+	}
+
+	public Poligono criarPoligono() {
+		return null;
 	}
 
 }
