288b68bdaf85885af4c394653ee4638078115b39
Thiago Rose <thiago.senhorinha@gmail.com>
2014-08-19 01:15:52 -0300

    Extraindo validações para classe Validador

---
+++ src/view/Validador.java
@@ -0,0 +1,44 @@
+package view;
+
+import java.awt.Color;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Validador {
+
+	public static final String REGEX_PARA_INTEIRO = "\\d+";
+
+	public static boolean validarInteiro(JTextField textField, JLabel label) {
+		String valor = textField.getText();
+		boolean semErro = true;
+		if (!(valor.matches(REGEX_PARA_INTEIRO))) {
+			marcarInvalido(label);
+			semErro = false;
+		} else {
+			marcarValido(label);
+		}
+		return semErro;
+	}
+
+	public static boolean validarString(JTextField textField, JLabel label) {
+		boolean semErro = true;
+		if (textField.getText() == null || textField.getText().isEmpty()) {
+			textField.setForeground(Color.RED);
+			marcarInvalido(label);
+			semErro = false;
+		} else {
+			marcarValido(label);
+		}
+		return semErro;
+	}
+
+	private static void marcarInvalido(JLabel label) {
+		label.setForeground(Color.RED);
+	}
+
+	private static void marcarValido(JLabel label) {
+		label.setForeground(Color.BLACK);
+	}
+
+}
