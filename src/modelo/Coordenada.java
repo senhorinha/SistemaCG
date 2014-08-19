72f58479787fbd70e4226826aca81d01ef765f68
Thiago Rose <thiago.senhorinha@gmail.com>
2014-08-19 15:31:00 -0300

    Adicionando setters

--- src/modelo/Coordenada.java
+++ src/modelo/Coordenada.java
@@ -13,8 +13,16 @@ public class Coordenada {
 		return this.x;
 	}
 
+	public void setX(int x) {
+		this.x = x;
+	}
+
 	public int getY() {
 		return this.y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
 	}
 
 }
