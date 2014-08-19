e17276a4364079e3aac7da6b9b4d6f6c7d611af1
Thiago Rose <thiago.senhorinha@gmail.com>
2014-08-19 16:10:10 -0300

    Retirando desenho teste

--- src/view/PainelDeDesenho.java
+++ src/view/PainelDeDesenho.java
@@ -5,28 +5,18 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-import modelo.Coordenada;
 import modelo.DisplayFile;
 import modelo.ObjetoGeometrico;
-import modelo.Reta;
 
 public class PainelDeDesenho extends JPanel {
-
-	public PainelDeDesenho() {
-		DisplayFile instance = DisplayFile.obterInstancia();
-		instance.adicionar(new Reta("reta", new Coordenada(10,10), new Coordenada(400,400)));
-	}
 
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		DisplayFile instance = DisplayFile.obterInstancia();
 		for (ObjetoGeometrico objetoGrafico : instance.getObjetos()) {
-			
 			objetoGrafico.desenhar(g, Color.RED);
 		}
 	}
-	
-	
 
 }
