f6c803c0e7414279b1d5c95d0420931110abf0e4
Rafinha88 <rafaelsm88@gmail.com>
2014-08-19 19:43:01 -0300

    Mudança no construtor da TransformacaoDeViewport

--- src/modelo/TransformacaoDeViewport.java
+++ src/modelo/TransformacaoDeViewport.java
@@ -7,25 +7,25 @@ public class TransformacaoDeViewport implements Transformacao {
 	private int xvpMin, yvpMin, xvpMax, yvpMax;
 	private int xwMin, ywMin, xwMax, ywMax;
 
-	public TransformacaoDeViewport(int xwMin, int ywMin, int xwMax, int ywMax) {
+	public TransformacaoDeViewport(int xvpMax, int yvpMax, int xwMax, int ywMax) {
 		this.yvpMin = 0;
 		this.yvpMin = 0;
-		this.xvpMax = 480;
-		this.yvpMax = 480;
+		this.xvpMax = xvpMax;
+		this.yvpMax = yvpMax;
 
-		this.xwMin = xwMin;
-		this.ywMin = ywMin;
+		this.xwMin = 0;
+		this.ywMin = 0;
 		this.xwMax = xwMax;
 		this.ywMax = ywMax;
 	}
 
 	@Override
-	public void executar(List<Coordenada> coordenada) {
+	public void executar(List<Coordenada> coordenadas) {
 		int xw;
 		int yw;
 		int xvp;
 		int yvp;
-		for (Coordenada c : coordenada) {
+		for (Coordenada c : coordenadas) {
 			xw = c.getX();
 			yw = c.getY();
 			xvp = (xw - xwMin) / (xwMax - xwMin) * (xvpMax - xvpMin);
