de8bcf395276dd4d42a0f4c50e22b4f392e58638
Thiago Rose <thiago.senhorinha@gmail.com>
2014-08-18 23:33:42 -0300

    Adicionando classe para o main

---
+++ src/controller/Executor.java
@@ -0,0 +1,14 @@
+package controller;
+
+import view.TelaPrincipal;
+
+public class Executor {
+
+	public static void main(String[] args) {
+
+		TelaPrincipal telaPrincipal = new TelaPrincipal();
+		telaPrincipal.setVisible(true);
+
+	}
+
+}
