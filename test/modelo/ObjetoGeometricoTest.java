268d308f386db69a7ebf3381cd5182c032820c99
Thiago Rose <thiago.senhorinha@gmail.com>
2014-08-19 14:59:02 -0300

    Adicionando source/test

---
+++ test/modelo/ObjetoGeometricoTest.java
@@ -0,0 +1,22 @@
+package modelo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ObjetoGeometricoTest {
+
+	@Test
+	public void test() {
+	}
+
+	@Test
+	public void deveSaberClonar() throws CloneNotSupportedException {
+		Coordenada a = new Coordenada(10, 10);
+		Coordenada b = new Coordenada(20, 20);
+		Reta reta = new Reta("Reta", a, b);
+		Reta retaClone = (Reta) reta.clone();
+		Assert.assertEquals(reta.getNome(), retaClone.getNome());
+		Assert.assertEquals(reta.obterCoordenadas(),
+				retaClone.obterCoordenadas());
+	}
+}
