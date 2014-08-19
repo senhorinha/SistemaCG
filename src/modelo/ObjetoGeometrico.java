dd71a6f0e30f9145fd92c41e17e06ce9ab72129e
Thiago Rose <thiago.senhorinha@gmail.com>
2014-08-19 14:59:38 -0300

    Implementando Cloneable

--- src/modelo/ObjetoGeometrico.java
+++ src/modelo/ObjetoGeometrico.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.List;
 
-public abstract class ObjetoGeometrico {
+public abstract class ObjetoGeometrico implements Cloneable {
 
 	String nome;
 	List<Coordenada> coordenadas;
@@ -12,7 +12,11 @@ public abstract class ObjetoGeometrico {
 	public abstract String getNome();
 
 	public abstract List<Coordenada> obterCoordenadas();
-	
+
 	public abstract void desenhar(Graphics grafico, Color cor);
 
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
 }
