d500e127691258de62469adbb0634254e56b0825
Thiago Rose <thiago.senhorinha@gmail.com>
2014-08-19 01:55:57 -0300

    Temp

--- src/view/PainelDeDesenho.java
+++ src/view/PainelDeDesenho.java
@@ -13,18 +13,17 @@ import modelo.Reta;
 public class PainelDeDesenho extends JPanel {
 
 	public PainelDeDesenho() {
-		DisplayFile.obterInstancia().adicionar(
-				new Reta("reta", new Coordenada(10, 10), new Coordenada(100,
-						100)));
+		DisplayFile instance = DisplayFile.obterInstancia();
+		instance.adicionar(new Reta("reta", new Coordenada(100, 100),
+				new Coordenada(400, 400)));
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawLine(100, 100, 500, 500);
-		for (ObjetoGeometrico objetoGrafico : DisplayFile.obterInstancia()
-				.getObjetos()) {
-			objetoGrafico.desenhar(g, Color.BLACK);
+		DisplayFile instance = DisplayFile.obterInstancia();
+		for (ObjetoGeometrico objetoGrafico : instance.getObjetos()) {
+			objetoGrafico.desenhar(g, Color.GREEN);
 		}
 	}
 
