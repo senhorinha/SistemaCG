3b6258b9bec5af569964f749f456b581e062f5f6
Thiago Rose <thiago.senhorinha@gmail.com>
2014-08-19 15:30:30 -0300

    Adicionando transformada de viewport

---
+++ src/modelo/TransformadaDeViewport.java
@@ -0,0 +1,38 @@
+package modelo;
+
+import java.util.List;
+
+public class TransformadaDeViewport implements Transformacao {
+
+	private int xvpMin, yvpMin, xvpMax, yvpMax;
+	private int xwMin, ywMin, xwMax, ywMax;
+
+	public TransformadaDeViewport(int xwMin, int ywMin, int xwMax, int ywMax) {
+		this.yvpMin = 0;
+		this.yvpMin = 0;
+		this.xvpMax = 480;
+		this.yvpMax = 480;
+
+		this.xwMin = xwMin;
+		this.ywMin = ywMin;
+		this.xwMax = xwMax;
+		this.ywMax = ywMax;
+	}
+
+	@Override
+	public void executar(List<Coordenada> coordenada) {
+		int xw;
+		int yw;
+		int xvp;
+		int yvp;
+		for (Coordenada c : coordenada) {
+			xw = c.getX();
+			yw = c.getY();
+			xvp = (xw - xwMin) / (xwMax - xwMin) * (xvpMax - xvpMin);
+			yvp = (1 - (yw - ywMin) / (ywMax - ywMin)) * (yvpMax - yvpMin);
+			c.setX(xvp);
+			c.setY(yvp);
+		}
+	}
+
+}
