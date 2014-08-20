1d05a40f8706172315b7c1b544bd3884d53ff2ec
Thiago Rose <thiago.senhorinha@gmail.com>
2014-08-19 22:49:56 -0300

    Implementando main

--- src/controller/Executor.java
+++ src/controller/Executor.java
@@ -6,8 +6,34 @@ public class Executor {
 
 	public static void main(String[] args) {
 
-		TelaPrincipal telaPrincipal = new TelaPrincipal();
-		telaPrincipal.setVisible(true);
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager
+					.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(TelaPrincipal.class.getName())
+					.log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(TelaPrincipal.class.getName())
+					.log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(TelaPrincipal.class.getName())
+					.log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(TelaPrincipal.class.getName())
+					.log(java.util.logging.Level.SEVERE, null, ex);
+		}
+
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new TelaPrincipal().setVisible(true);
+			}
+		});
 
 	}
 
