91e87b34c1421a3704a947bf4230a82453c37f52
Rafinha88 <rafaelsm88@gmail.com>
2014-08-21 09:58:33 -0300

    Criado metodo obterCentroGeometrico

--- src/modelo/ObjetoGeometrico.java
+++ src/modelo/ObjetoGeometrico.java
@@ -30,6 +30,17 @@ public abstract class ObjetoGeometrico implements Cloneable {
 	public void transformarCoordenadas(Transformacao transformacao) {
 		transformacao.executar(this.coordenadas);
 	}
+	
+	public Coordenada getCentroGeometrico() {
+		int x = 0, y = 0;
+		for (Coordenada coordenada : coordenadas) {
+			x = x + coordenada.getX();
+			y = y + coordenada.getY();
+		}
+		x = x / coordenadas.size();
+		y = y / coordenadas.size();
+		return new Coordenada(x, y);
+	}
 
 	@Override
 	public Object clone() throws CloneNotSupportedException {
