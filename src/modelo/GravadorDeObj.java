4c7450f0b6787f1ca2914b1fa15e3659460bb232
Thiago Rose <thiago.senhorinha@gmail.com>
2014-08-21 23:31:00 -0300

    Adicionando classe para gravar obj

---
+++ src/modelo/GravadorDeObj.java
@@ -0,0 +1,47 @@
+package modelo;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class GravadorDeObj {
+
+	private FileWriter escritor;
+	private String caractereDeNovaLinha;
+
+	public void executar(String caminhoDoArquivo, List<ObjetoGeometrico> objetos) throws IOException {
+		escritor = new FileWriter(caminhoDoArquivo);
+		caractereDeNovaLinha = System.getProperty("line.separator");
+		for (ObjetoGeometrico objeto : objetos) {
+			gravarNome(objeto.nome);
+			gravarCoordenadas(objeto.coordenadas);
+			gravarFaceta(objeto.coordenadas.size());
+		}
+		escritor.close();
+	}
+
+	private void gravarFaceta(int numeroDeCoordenadas) throws IOException {
+		int indice;
+		escritor.write(ComandoObj.FACETA.toString().concat(" "));
+		for (int i = numeroDeCoordenadas; i != 0; i--) {
+			indice = (i * -1);
+			if (indice == 0) {
+				escritor.write(String.valueOf(indice).concat(caractereDeNovaLinha));
+			} else {
+				escritor.write(String.valueOf(indice).concat(" "));
+			}
+		}
+	}
+
+	private void gravarCoordenadas(List<Coordenada> coordenadas) throws IOException {
+		for (Coordenada coordenada : coordenadas) {
+			String coordenadaX = String.valueOf(coordenada.getX());
+			String coordenadaY = String.valueOf(coordenada.getY());
+			escritor.write(ComandoObj.VERTICE.toString().concat(" ").concat(coordenadaX).concat(" ").concat(coordenadaY).concat(caractereDeNovaLinha));
+		}
+	}
+
+	private void gravarNome(String nome) throws IOException {
+		escritor.write(ComandoObj.NOME.toString().concat(" ").concat(nome).concat(caractereDeNovaLinha));
+	}
+}
