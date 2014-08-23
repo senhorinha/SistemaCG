a19d4bbfb05ab15da3c4cc7e0e4ea3e274f0dd63
Rafinha88 <rafaelsm88@gmail.com>
2014-08-23 19:07:25 -0300

    Criando o RotacionadorArbitrario

---
+++ src/modelo/RotacionadorArbitrario.java
@@ -0,0 +1,37 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RotacionadorArbitrario extends Movimentador {
+
+	int angulo;
+	double seno, cosseno, radiano;
+
+	public RotacionadorArbitrario(ObjetoGeometrico objeto, Coordenada vetor, int angulo) {
+		super(objeto, vetor);
+		this.angulo = angulo;
+	}
+
+	@Override
+	public void movimentar() {
+		radiano = Math.toRadians(angulo);
+		seno = Math.sin(radiano);
+		cosseno = Math.cos(radiano);
+		int dx = coordenadaDeReferencia.getX();
+		int dy = coordenadaDeReferencia.getY();
+
+		List<Coordenada> novasCoordenadas = new ArrayList<Coordenada>();
+
+		for (Coordenada coordenada : objeto.getCoordenadas()) {
+			novasCoordenadas.add(new Coordenada(
+					(int) ( (coordenada.getX() - dx) * cosseno + (coordenada.getY() - dy) * seno + dx ),
+					(int) ( (coordenada.getX() - dx) * -seno + (coordenada.getY() - dy) * cosseno + dy ) ));
+
+		}
+
+		objeto.setCoordenadas(novasCoordenadas);
+
+	}
+
+}
