dedef9d3b951fceeb38bfacac34beb2c41143955
Thiago Rose <thiago.senhorinha@gmail.com>
2014-09-14 12:33:58 -0300

    Adicionando testes para clipping de reta

---
+++ test/modelo/utils/ClipadorTestParaReta.java
@@ -0,0 +1,103 @@
+package modelo.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.Coordenada;
+import modelo.Window;
+import modelo.objetos.ObjetoGeometrico;
+import modelo.objetos.Reta;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClipadorTestParaReta {
+
+	private Window window;
+	private Clipador clipador;
+	List<ObjetoGeometrico> objetos;
+	private ObjetoGeometrico objeto;
+
+	@Before
+	public void setup() {
+		window = new Window(100, 100);
+		clipador = new Clipador();
+		objetos = new ArrayList<ObjetoGeometrico>();
+	}
+
+	public void criarRetaAdicionarNaListaExecutarClipador(double x1, double y1, double x2, double y2) {
+		objeto = new Reta("teste", null, new Coordenada(x1, y1), new Coordenada(x2, y2));
+		objetos.add(objeto);
+		objetos = clipador.executar(objetos, window);
+	}
+
+	@Test
+	public void deve_saber_identificar_reta_dentro_da_window() {
+		criarRetaAdicionarNaListaExecutarClipador(10, 10, 10, 10);
+		Assert.assertFalse(objetos.isEmpty());
+	}
+
+	@Test
+	public void deve_saber_identificar_reta_fora_da_window() {
+		criarRetaAdicionarNaListaExecutarClipador(110, 110, 110, 110);
+		Assert.assertTrue(objetos.isEmpty());
+	}
+
+	@Test
+	public void deve_saber_clipar_a_regiao_norte_da_reta() {
+		criarRetaAdicionarNaListaExecutarClipador(10, 10, 10, 200);
+		Coordenada coordenadaClipada = objetos.get(0).getCoordenadas().get(1);
+		assertEquals(10, (int) coordenadaClipada.getX());
+		assertEquals(100, (int) coordenadaClipada.getY());
+	}
+
+	@Test
+	public void deve_saber_clipar_a_regiao_sul_da_reta() {
+		criarRetaAdicionarNaListaExecutarClipador(10, 10, 10, -10);
+		Coordenada coordenadaClipada = objetos.get(0).getCoordenadas().get(1);
+		assertEquals(10, (int) coordenadaClipada.getX());
+		assertEquals(0, (int) coordenadaClipada.getY());
+	}
+
+	@Test
+	public void deve_saber_clipar_a_regiao_oeste_da_reta() {
+		criarRetaAdicionarNaListaExecutarClipador(10, 10, 200, 10);
+		Coordenada coordenadaClipada = objetos.get(0).getCoordenadas().get(1);
+		assertEquals(100, (int) coordenadaClipada.getX());
+		assertEquals(10, (int) coordenadaClipada.getY());
+	}
+
+	@Test
+	public void deve_saber_clipar_a_regiao_leste_da_reta() {
+		criarRetaAdicionarNaListaExecutarClipador(10, 10, 200, 10);
+		Coordenada coordenadaClipada = objetos.get(0).getCoordenadas().get(1);
+		assertEquals(100, (int) coordenadaClipada.getX());
+		assertEquals(10, (int) coordenadaClipada.getY());
+	}
+
+	@Test
+	public void deve_saber_clipar_a_regiao_oeste_e_leste_da_reta() {
+		criarRetaAdicionarNaListaExecutarClipador(-100, 10, 200, 10);
+		Coordenada cordenadaClipadaNoOeste = objetos.get(0).getCoordenadas().get(0);
+		Coordenada cordenadaClipadaNoLeste = objetos.get(0).getCoordenadas().get(1);
+		assertEquals(0, (int) cordenadaClipadaNoOeste.getX());
+		assertEquals(10, (int) cordenadaClipadaNoOeste.getY());
+		assertEquals(100, (int) cordenadaClipadaNoLeste.getX());
+		assertEquals(10, (int) cordenadaClipadaNoLeste.getY());
+	}
+
+	@Test
+	public void deve_saber_clipar_a_regiao_sul_e_norte_da_reta() {
+		criarRetaAdicionarNaListaExecutarClipador(10, -100, 10, 200);
+		Coordenada cordenadaClipadaNoOeste = objetos.get(0).getCoordenadas().get(0);
+		Coordenada cordenadaClipadaNoLeste = objetos.get(0).getCoordenadas().get(1);
+		assertEquals(10, (int) cordenadaClipadaNoOeste.getX());
+		assertEquals(0, (int) cordenadaClipadaNoOeste.getY());
+		assertEquals(10, (int) cordenadaClipadaNoLeste.getX());
+		assertEquals(100, (int) cordenadaClipadaNoLeste.getY());
+	}
+
+}
