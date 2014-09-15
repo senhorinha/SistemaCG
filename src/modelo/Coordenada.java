827343ee83ed73ea3be0b771883a40198eefe8b3
Thiago Rose <thiago.senhorinha@gmail.com>
2014-09-14 22:44:35 -0300

    Implementando equals e hashcode

--- src/modelo/Coordenada.java
+++ src/modelo/Coordenada.java
@@ -24,14 +24,41 @@ public class Coordenada {
 	public void setY(double y) {
 		this.y = y;
 	}
-	
+
 	public void somarAoX(int valor) {
 		x += valor;
 	}
-	
+
 	public void somarAoY(int valor) {
 		y += valor;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Coordenada other = (Coordenada) obj;
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+			return false;
+		if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
+			return false;
+		return true;
+	}
 
 }
